dce6035ff9cbb02cf9a6a5d3d0a114963f33ff38
Dmitry Dmitrenko <752told@gmail.com>
2026-06-22 00:53:14 +0300

    changed style work

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.scss'
 import {initTheme, observer} from './theme'
 import {setLanguage, renderTranslations, initLangDropdown, updateLanguageDropdown} from './i18'
 import {renderLayout} from "./parts";
