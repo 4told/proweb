7daaaf4d7ed5f0f94c488c014b2fa9f5412e8457
Dmitry Dmitrenko <752told@gmail.com>
2026-06-25 19:02:56 +0300

    removed testimonials block

--- src/parts/index.ts
+++ src/parts/index.ts
@@ -1,11 +1,10 @@
 import {renderHeader} from "./header.ts";
 import {renderHero} from "./hero.ts";
-import {renderWhatWeDo} from "./what-we-do.ts";
+import {renderWhatWeDo} from "./../modules/solutions/solutions.ts";
 import {renderSelectedWork} from "./selected-work.ts";
 import {renderHowItHappens} from "./how-it-happens.ts";
 import {renderContact} from "./contact.ts";
 import {renderAbout} from "./about.ts";
-import {renderTestimonials} from "./testimonials.ts";
 import {renderFooter} from "./footer.ts";
 export function renderLayout() {
     return `
@@ -16,7 +15,6 @@ export function renderLayout() {
     ${renderHowItHappens()}
     ${renderContact()}
     ${renderAbout()}
-    ${renderTestimonials()}
     ${renderFooter()}
   `
 }
